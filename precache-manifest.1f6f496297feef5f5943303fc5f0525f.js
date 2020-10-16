980eb80e6775c31426cc45760f7be4cd5dcb5a31
Giovanni Pires da Silva <giovannipds@gmail.com>
2020-10-16 20:04:06 -0300

    Updates

---
+++ precache-manifest.1f6f496297feef5f5943303fc5f0525f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c043ebd2159098d4eab20091fba215a5",
+    "url": "/nuorder-frontend-challenge/index.html"
+  },
+  {
+    "revision": "5a91211d26ee7f299859",
+    "url": "/nuorder-frontend-challenge/static/css/main.16039916.chunk.css"
+  },
+  {
+    "revision": "276de40f5ec05d46b67d",
+    "url": "/nuorder-frontend-challenge/static/js/2.e75835f5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/nuorder-frontend-challenge/static/js/2.e75835f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a91211d26ee7f299859",
+    "url": "/nuorder-frontend-challenge/static/js/main.d8854922.chunk.js"
+  },
+  {
+    "revision": "77efc9144e6bc67a7c23",
+    "url": "/nuorder-frontend-challenge/static/js/runtime-main.c48d2d69.js"
+  },
+  {
+    "revision": "23779e18eee90d57a355454cac1c02de",
+    "url": "/nuorder-frontend-challenge/static/media/logo.23779e18.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/nuorder-frontend-challenge/precache-manifest.1f6f496297feef5f5943303fc5f0525f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nuorder-frontend-challenge/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
